e60e257f84b3871c4008493d1c6dc29cd43858e4
princeoncada <prince.oncada@gmail.com>
2026-05-25 18:59:54 -0400

    style: canonicalize match activity classes

--- src/components/activities/MatchWordActivity.jsx
+++ src/components/activities/MatchWordActivity.jsx
@@ -2,8 +2,8 @@ export function MatchWordActivity() {
   return (
     <>
       {/* ZONE: ACTIVITY 3, MATCH THE WORD. Edit .c3, .match-desc, .match-pic, .dot, .match-word. */}
-                <section className="card c3 top-[479px] left-[30px] w-[363px] h-[228px]">
-                  <div className="label top-[-16px] left-1">
+                <section className="card c3 top-119.75 left-7.5 w-90.75 h-57">
+                  <div className="label -top-4 left-1">
                     <div className="num-tab"><svg viewBox="0 0 58 36">
                         <path className="white" d="M2 2H43L56 18 43 34H2Q8 18 2 2Z" />
                       </svg><span>3</span></div>
