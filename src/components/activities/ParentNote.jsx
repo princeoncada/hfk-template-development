273d8e75b29652ebedd38e2eb5b65c614c4aa09d
princeoncada <prince.oncada@gmail.com>
2026-05-25 18:59:54 -0400

    style: canonicalize parent note classes

--- src/components/activities/ParentNote.jsx
+++ src/components/activities/ParentNote.jsx
@@ -1,11 +1,11 @@
 export function ParentNote() {
   return (
     <>
-      <section className="parent-card top-[737px] left-[614px] w-[174px] h-[168px] pt-[18px]">
-                  <div className="parent-banner absolute top-[-17px] left-[16px]">PARENT NOTE</div>
+      <section className="parent-card top-184.25 left-153.5 w-43.5 h-42 pt-4.5">
+                  <div className="parent-banner absolute -top-4.25 left-4">PARENT NOTE</div>
                   <div className="parent-text text-[17px] leading-[25px] mt-0">Encourage your child to sound out each word and read with confidence. Small steps lead to
                     big success!</div>
-                  <svg className="parent-heart size-6 left-[74px] bottom-[3px]">
+                  <svg className="parent-heart size-6 left-18.5 bottom-0.75">
                     <use href="#heart" />
                   </svg>
                 </section>
