f85efb0d92a65087007f0db616e671740bfbf6d2
princeoncada <prince.oncada@gmail.com>
2026-05-25 19:01:10 -0400

    style: canonicalize consonant activity classes

--- src/components/activities/BeginningConsonantActivity.jsx
+++ src/components/activities/BeginningConsonantActivity.jsx
@@ -2,28 +2,28 @@ export function BeginningConsonantActivity() {
   return (
     <>
       {/* ZONE: ACTIVITY 2, CIRCLE THE BEGINNING CONSONANT. Edit .c2, .letters-row, .letter positions. */}
-                <section className="card c2 top-47.25 h-66 left-104.5 w-92.5">
-                  <div className="label -top-4 left-1">
-                    <div className="num-tab"><svg viewBox="0 0 58 36">
-                        <path className="white" d="M2 2H43L56 18 43 34H2Q8 18 2 2Z" />
-                      </svg><span>2</span></div>
-                    <div className="label-title tracking-tight">CIRCLE THE BEGINNING CONSONANT</div>
-                  </div>
-                  <div className="subtext absolute left-6.25 top-9.5 w-77.5 text-center text-[16.5px] leading-[1.05] whitespace-nowrap">
-                    Circle the beginning consonant in each word.</div>
-                  <div className="letters-row lr1"><svg className="pic">
-                      <use href="#dog" />
-                    </svg><span className="letter l1">d</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
-                  <div className="letters-row lr2"><svg className="pic">
-                      <use href="#log" />
-                    </svg><span className="letter l1">l</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
-                  <div className="letters-row lr3"><svg className="pic">
-                      <use href="#mop" />
-                    </svg><span className="letter l1">m</span><span className="letter l2">o</span><span className="letter l3">p</span></div>
-                  <svg className="pencil-small">
-                    <use href="#pencil" />
-                  </svg>
-                </section>
+      <section className="card c2 top-47.25 h-66 left-104.5 w-92.5">
+        <div className="label -top-4 left-1">
+          <div className="num-tab"><svg viewBox="0 0 58 36">
+            <path className="white" d="M2 2H43L56 18 43 34H2Q8 18 2 2Z" />
+          </svg><span>2</span></div>
+          <div className="label-title tracking-tight">CIRCLE THE BEGINNING CONSONANT</div>
+        </div>
+        <div className="subtext absolute left-6.25 top-9.5 w-77.5 text-center text-[16.5px] leading-[1.05] whitespace-nowrap">
+          Circle the beginning consonant in each word.</div>
+        <div className="letters-row lr1"><svg className="pic">
+          <use href="#dog" />
+        </svg><span className="letter l1">d</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
+        <div className="letters-row lr2"><svg className="pic">
+          <use href="#log" />
+        </svg><span className="letter l1">l</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
+        <div className="letters-row lr3"><svg className="pic">
+          <use href="#mop" />
+        </svg><span className="letter l1">m</span><span className="letter l2">o</span><span className="letter l3">p</span></div>
+        <svg className="pencil-small">
+          <use href="#pencil" />
+        </svg>
+      </section>
     </>
   );
 }
