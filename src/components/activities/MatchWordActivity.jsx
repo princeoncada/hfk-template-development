b1c9366ce25216baaf5f8820e47e004d74ceabe0
princeoncada <prince.oncada@gmail.com>
2026-05-25 18:53:53 -0400

    refactor: extract match word activity

---
+++ src/components/activities/MatchWordActivity.jsx
@@ -0,0 +1,29 @@
+export function MatchWordActivity() {
+  return (
+    <>
+      {/* ZONE: ACTIVITY 3, MATCH THE WORD. Edit .c3, .match-desc, .match-pic, .dot, .match-word. */}
+                <section className="card c3 top-[479px] left-[30px] w-[363px] h-[228px]">
+                  <div className="label top-[-16px] left-1">
+                    <div className="num-tab"><svg viewBox="0 0 58 36">
+                        <path className="white" d="M2 2H43L56 18 43 34H2Q8 18 2 2Z" />
+                      </svg><span>3</span></div>
+                    <div className="label-title">MATCH THE WORD</div>
+                  </div>
+                  <div className="match-desc">Draw a line to match each picture to its word.</div>
+                  <svg className="match-pic m1">
+                    <use href="#dog" />
+                  </svg><svg className="match-pic m2">
+                    <use href="#log" />
+                  </svg><svg className="match-pic m3">
+                    <use href="#mop" />
+                  </svg>
+                  <span className="dot left-dot md1"></span><span className="dot left-dot md2"></span><span className="dot left-dot md3"></span>
+                  <span className="dot right-dot md1"></span><span className="dot right-dot md2"></span><span
+                    className="dot right-dot md3"></span>
+                  <div className="match-word mw1">mop</div>
+                  <div className="match-word mw2">dog</div>
+                  <div className="match-word mw3">log</div>
+                </section>
+    </>
+  );
+}
