6f77d423a6907322d80658ef38f8da8443f94ce9
princeoncada <prince.oncada@gmail.com>
2026-05-25 18:59:54 -0400

    style: canonicalize reading check classes

--- src/components/activities/ReadingCheckActivity.jsx
+++ src/components/activities/ReadingCheckActivity.jsx
@@ -2,8 +2,8 @@ export function ReadingCheckActivity() {
   return (
     <>
       {/* ZONE: ACTIVITY 4, READING CHECK. Edit .c4, .check-desc, .check-row. */}
-                <section className="card c4 top-[480px] left-[406px] h-[228px] w-[380px]">
-                  <div className="label top-[-16px] left-1">
+                <section className="card c4 top-120 left-101.5 h-57 w-95">
+                  <div className="label -top-4 left-1">
                     <div className="num-tab"><svg viewBox="0 0 58 36">
                         <path className="white" d="M2 2H43L56 18 43 34H2Q8 18 2 2Z" />
                       </svg><span>4</span></div>
