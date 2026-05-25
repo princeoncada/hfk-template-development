48881c615ecfc07673588e1cd7ea34ccee0a6eeb
princeoncada <prince.oncada@gmail.com>
2026-05-25 19:20:42 -0400

    style: adjust layout and positioning of elements in BeginningConsonantActivity

--- src/components/activities/BeginningConsonantActivity.jsx
+++ src/components/activities/BeginningConsonantActivity.jsx
@@ -9,18 +9,33 @@ export function BeginningConsonantActivity() {
           </svg><span>2</span></div>
           <div className="label-title tracking-tight">CIRCLE THE BEGINNING CONSONANT</div>
         </div>
-        <div className="subtext absolute left-6.25 top-9.5 w-77.5 text-center text-[16.5px] leading-[1.05] whitespace-nowrap">
+        <div className="subtext absolute w-77.5 text-center text-[16.5px] leading-[1.05] whitespace-nowrap top-[27px] left-[34px] tracking-[1.2px]">
           Circle the beginning consonant in each word.</div>
-        <div className="letters-row lr1"><svg className="pic">
-          <use href="#dog" />
-        </svg><span className="letter l1">d</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
-        <div className="letters-row lr2"><svg className="pic">
-          <use href="#log" />
-        </svg><span className="letter l1">l</span><span className="letter l2">o</span><span className="letter l3">g</span></div>
-        <div className="letters-row lr3"><svg className="pic">
-          <use href="#mop" />
-        </svg><span className="letter l1">m</span><span className="letter l2">o</span><span className="letter l3">p</span></div>
-        <svg className="pencil-small">
+        <div className="letters-row lr1">
+          <svg className="pic top-[-27px] left-[-8px]">
+            <use href="#dog" />
+          </svg>
+          <span className="letter l1 top-[-10px] left-[118px]">d</span>
+          <span className="letter l2 top-[-10px] left-[193px]">o</span>
+          <span className="letter l3 top-[-10px] left-[264px]">g</span>
+        </div>
+        <div className="letters-row lr2">
+          <svg className="pic left-[-8px]">
+            <use href="#log" />
+          </svg>
+          <span className="letter l1 left-[123px]">l</span>
+          <span className="letter l2 left-[193px]">o</span>
+          <span className="letter l3 left-[263px]">g</span>
+        </div>
+        <div className="letters-row lr3">
+          <svg className="pic left-[-8px] top-[-6px]">
+            <use href="#mop" />
+          </svg>
+          <span className="letter l1 top-[13px] left-[115px]">m</span>
+          <span className="letter l2 top-[13px] left-[193px]">o</span>
+          <span className="letter l3 top-[13px] left-[264px]">p</span>
+        </div>
+        <svg className="pencil-small bottom-[2px] right-[6px] rotate-0 size-13">
           <use href="#pencil" />
         </svg>
       </section>
