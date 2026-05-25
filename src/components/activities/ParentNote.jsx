923287f0f47325bc43cf51c16cff5f5639d59e0a
princeoncada <prince.oncada@gmail.com>
2026-05-25 18:53:53 -0400

    refactor: extract parent note

---
+++ src/components/activities/ParentNote.jsx
@@ -0,0 +1,14 @@
+export function ParentNote() {
+  return (
+    <>
+      <section className="parent-card top-[737px] left-[614px] w-[174px] h-[168px] pt-[18px]">
+                  <div className="parent-banner absolute top-[-17px] left-[16px]">PARENT NOTE</div>
+                  <div className="parent-text text-[17px] leading-[25px] mt-0">Encourage your child to sound out each word and read with confidence. Small steps lead to
+                    big success!</div>
+                  <svg className="parent-heart size-6 left-[74px] bottom-[3px]">
+                    <use href="#heart" />
+                  </svg>
+                </section>
+    </>
+  );
+}
