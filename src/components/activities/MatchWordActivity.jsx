2f07be3b95bfefce3185f69f694398f6e5dafda0
princeoncada <prince.oncada@gmail.com>
2026-05-25 20:45:50 -0400

    style: enhance layout and positioning of elements in MatchWordActivity

--- src/components/activities/MatchWordActivity.jsx
+++ src/components/activities/MatchWordActivity.jsx
@@ -9,20 +9,25 @@ export function MatchWordActivity() {
           </svg><span>3</span></div>
           <div className="label-title">MATCH THE WORD</div>
         </div>
-        <div className="match-desc">Draw a line to match each picture to its word.</div>
-        <svg className="match-pic m1">
+        <div className="match-desc top-[24px] left-[22px] tracking-[1.4px] w-67">Draw a line to match each picture to its word.</div>
+        <svg className="match-pic m1 top-[57px] left-[12px]">
           <use href="#dog" />
-        </svg><svg className="match-pic m2">
+        </svg>
+        <svg className="match-pic m2 top-[112px] left-[14px]">
           <use href="#log" />
-        </svg><svg className="match-pic m3">
+        </svg>
+        <svg className="match-pic m3 top-[161px] left-[10px] rotate-7">
           <use href="#mop" />
         </svg>
-        <span className="dot left-dot md1"></span><span className="dot left-dot md2"></span><span className="dot left-dot md3"></span>
-        <span className="dot right-dot md1"></span><span className="dot right-dot md2"></span><span
-          className="dot right-dot md3"></span>
-        <div className="match-word mw1">mop</div>
-        <div className="match-word mw2">dog</div>
-        <div className="match-word mw3">log</div>
+        <span className="dot left-dot md1 top-[79px] left-[103px]"></span>
+        <span className="dot left-dot md2 top-[135px] left-[103px]"></span>
+        <span className="dot left-dot md3 top-[189px] left-[103px]"></span>
+        <span className="dot right-dot md1 top-[79px] left-[240px]"></span>
+        <span className="dot right-dot md2 top-[135px] left-[240px]"></span>
+        <span className="dot right-dot md3 top-[189px] left-[240px]"></span>
+        <div className="match-word mw1 tracking-[4px] top-[62px] left-[278px]">mop</div>
+        <div className="match-word mw2 tracking-[4px] top-[117px] left-[280px]">dog</div>
+        <div className="match-word mw3 tracking-[4px] top-[174px] left-[280px]">log</div>
       </section>
     </>
   );
