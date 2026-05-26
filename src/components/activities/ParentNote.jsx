9b6f4bd2dbbb1f9fd4e25dd9c289c891a846ccfa
princeoncada <prince.oncada@gmail.com>
2026-05-25 21:06:05 -0400

    style: adjust font size and leading of text in ParentNote component

--- src/components/activities/ParentNote.jsx
+++ src/components/activities/ParentNote.jsx
@@ -3,7 +3,7 @@ export function ParentNote() {
     <>
       <section className="parent-card top-184.25 left-153.5 w-43.5 h-42 pt-4.5">
         <div className="parent-banner absolute -top-4.25 left-4">PARENT NOTE</div>
-        <div className="parent-text text-[17px] leading-6.25 mt-0">Encourage your child to sound out each word and read with confidence. Small steps lead to
+        <div className="parent-text text-[16px] tracking-[0.7px] leading-[20.4px] mt-0">Encourage your child to sound out each word and read with confidence. Small steps lead to
           big success!</div>
         <svg className="parent-heart size-6 left-18.5 bottom-0.75">
           <use href="#heart" />
